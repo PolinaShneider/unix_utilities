16fc5f0f5a12e4a65d5f4f71d26994fb70a8ad49
Joel Moniz <jramoniz@gmail.com>
2015-11-22 19:16:29 +0530

    Adding in support for groups

--- select_server.c
+++ select_server.c
@@ -133,6 +133,26 @@ struct fd_linkedlist *new_fd_linkedlist_member(int acc_conn) {
   struct fd_linkedlist *new_mem = (struct fd_linkedlist *) malloc(sizeof(struct fd_linkedlist));
   new_mem->fd = acc_conn;
   new_mem->next = NULL;
+  return new_mem;
+}
+
+struct group_linkedlist *new_grp_linkedlist_member(struct fd_linkedlist *fd_mem, char *group_name) {
+  struct group_linkedlist *new_mem = (struct group_linkedlist *) malloc(sizeof(struct group_linkedlist));
+  new_mem->fd = fd_mem;
+
+  new_mem->group_name = (char *) malloc(sizeof(char) * (strlen(group_name) + 1));
+  strcpy(new_mem->group_name, group_name);
+
+  new_mem->group_name[strlen(group_name)] = '\0';
+
+  if (DEBUG) {
+    printf("Length: %d (old) vs %d (new)\n", (int)strlen(group_name), (int)strlen(new_mem->group_name));
+    printf("Old group name: %s\n", group_name);
+    printf("New group name: %s\n", new_mem->group_name);
+  }
+
+  new_mem->next = NULL;
+  return new_mem;
 }
 
 void delete_fd_linkedlist_member(struct fd_linkedlist *mem) {
@@ -160,17 +180,6 @@ void delete_fd_linkedlist_member(struct fd_linkedlist *mem) {
   }
 }
 
-void receive_termination_one(int fd) {
-  int nw_one;
-  read(fd, &nw_one, sizeof(nw_one));
-
-  if (DEBUG)
-    printf("Received: %d\n", ntohs(nw_one));
-
-  if (ntohs(nw_one) != 1)
-    printf("Warning: Received termination %d instead of termination 1\n", ntohs(nw_one));
-}
-
 void close_parent_fds(int listening_fd) {
   int i;
 
@@ -182,9 +191,127 @@ void close_parent_fds(int listening_fd) {
     perror("Error closing TCP socket");
 }
 
-void close_child_fd() {
-  if (close(unix_domain_sockets[child][1]) == -1)
-    perror("Error closing UDS in child");
+void add_to_group(char *group_name, struct fd_linkedlist *add_fd) {
+
+  struct group_linkedlist *i_grp = groups;
+
+  if (DEBUG)
+    printf("Add group name: %s\n", group_name);
+
+  while (i_grp != NULL && i_grp->next != NULL) {
+    if (!strcmp(group_name, i_grp->group_name)) {
+
+      struct fd_linkedlist *i_fd = i_grp->fd;
+
+      if (i_fd->fd == add_fd->fd)
+        return;
+
+      while (i_fd -> next != NULL) {
+        i_fd = i_fd->next;
+
+        if (i_fd->fd == add_fd->fd)
+          return;
+      }
+
+      i_fd->next = add_fd;
+      return;
+    }
+    else {
+      i_grp = i_grp->next;
+    }
+  }
+
+  if (i_grp == NULL) {
+
+    if (DEBUG)
+      printf("Adding new group %s to NULL list\n", group_name);
+
+    groups = new_grp_linkedlist_member(add_fd, group_name);
+  }
+  else {
+
+    if (DEBUG)
+      printf("Adding new group %s to non-NULL list\n", group_name);
+
+    i_grp->next = new_grp_linkedlist_member(add_fd, group_name);
+  }
+
+}
+
+void send_to_fd_list(char *buff, struct fd_linkedlist *i_fd) {
+
+  if (DEBUG)
+    printf("Sending to list: %s", buff);
+
+  while (i_fd != NULL) {
+    send (i_fd->fd, buff, strlen (buff), 0);
+    i_fd = i_fd->next;
+  }
+}
+
+void send_to_all(char *buff) {
+  send_to_fd_list(buff, fds);
+}
+
+void send_msg_to_group(char *grp_msg_buff) {
+  struct group_linkedlist *i_grp = groups;
+  int i;
+  for (i = 0; grp_msg_buff[i] != '$' && i < strlen(grp_msg_buff); i++);
+
+  if (DEBUG) {
+    printf("Groupname: ");
+    int j;
+
+    for (j = 0; j < i; j++)
+      printf("%c", grp_msg_buff[j]);
+
+    printf(" Length: %d Message: %s", i, grp_msg_buff + i + 1);
+
+  }
+
+  while (i_grp != NULL) {
+    if (DEBUG) {
+      printf("Comparing first %d of %s and %s\n", i, grp_msg_buff, i_grp->group_name);
+    }
+    if (!strncmp(grp_msg_buff, i_grp->group_name, i) && (strlen(i_grp->group_name) == i)) {
+      if (DEBUG)
+        printf("Here\n");
+
+      send_to_fd_list(grp_msg_buff + i + 1, i_grp->fd);
+      break;
+    }
+    else {
+
+      if (DEBUG)
+        printf("Values: %d, %s, %d\n", strncmp(grp_msg_buff, i_grp->group_name, i), i_grp->group_name, i);
+      i_grp = i_grp->next;
+    }
+  }
+}
+
+void handle_message(char *buff, struct fd_linkedlist *i_fd) {
+  if (!strncmp(buff, "GROUP$", 6)) {
+
+    int i;
+    for (i = 0; i < strlen(buff); i++) {
+      if (buff[i] == '\n' || buff[i] == '\r') {
+        buff[i] = '\0';
+        break;
+      }
+    }
+
+    if (DEBUG) {
+      printf("A1dding %d to group %s yep\n", i_fd->fd, buff + 6);
+    }
+    add_to_group(buff + 6, i_fd);
+  }
+  else if (!strncmp(buff, "GROUPMSG$", 9)) {
+    send_msg_to_group(buff + 9);
+  }
+  else {
+    send_to_all(buff);
+  }
+
 }
 
 int main(int argc, char **argv) {
@@ -295,13 +422,16 @@ int main(int argc, char **argv) {
           else
             first_run_done = 1;
 
-          if (buff[rcv_size - 1] == '\n')
-            buff[rcv_size - 1] = '\0';
-          else
-            buff[rcv_size] = '\0';
+          // if (buff[rcv_size - 1] == '\n')
+          //   buff[rcv_size - 1] = '\0';
+          // else
+
+          buff[rcv_size] = '\0';
 
           if (DEBUG)
-            printf("Received %d bytes from client %d: %s\n", rcv_size, i_fd->fd, buff);
+            printf("Received %d bytes from client %d: %s yep\n", rcv_size, i_fd->fd, buff);
+
+          handle_message(buff, i_fd);
 
         }
 
